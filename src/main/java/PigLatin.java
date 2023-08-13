3cf03b0e51315aba6456bf9f5b3acd21e3755450
Christopher King <king.cdk@gmail.com>
2023-08-13 17:32:09 +0000

    Added logic to PigLatin

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,8 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String firstChar = in.substring(0,1);
+        String wordBody = in.substring(1, in.length());
+        return wordBody + firstChar + "ay";
     }
 }
